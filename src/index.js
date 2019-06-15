8183df5f3426c687fadf0d6f5851a8179ce8bae0
singhtalvinder <talvindersingh.ca@gmail.com>
2019-06-15 13:16:53 -0400

    Fixed port case

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app)
 
 const io = socketio(server)
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const publicDirPath = path.join(__dirname,'../public')
 
